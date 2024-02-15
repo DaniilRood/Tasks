bf4425111016885561041deedcf0e16c387bdb55
Daniil Lysykh <lysykh_daniil@mail.ru>
2024-02-15 19:28:37 +0300

    3

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 5;
-        if (a == 4) {
+        int a = 3;
             System.out.println("Hello world!");
         }
     }
